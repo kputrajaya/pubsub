5049c215c87ae93547904bb9162a337ed4805a57
Kevin Putrajaya <freakperzon@yahoo.com>
2025-03-14 00:38:39 +0700

    Initial commit

---
+++ sdk.js
@@ -0,0 +1,86 @@
+class PubSub {
+  constructor(options = {}) {
+    const {
+      host = 'localhost',
+      appKey = 'foo',
+      subKeyParam = 'k',
+      getData = () => ({}),
+      setData = console.log,
+      pubFrequency = 1000,
+      subFrequency = 10000,
+      reconnectDelay = 1000,
+    } = options;
+
+    let ws;
+    let lastJson;
+    let pubInt;
+    let subInt;
+
+    const getParam = (name) => {
+      try {
+        const params = new URLSearchParams(window.location.search);
+        return params.get(name);
+      } catch (err) {
+        console.error('Error parsing params:', err);
+        return null;
+      }
+    };
+    const clearIntervals = () => {
+      clearInterval(pubInt);
+      clearInterval(subInt);
+    };
+    const connect = () => {
+      lastJson = null;
+      clearIntervals();
+      pubInt = setInterval(this.pub, pubFrequency);
+      subInt = setInterval(this.sub, subFrequency);
+
+      ws = new WebSocket(`wss://${host}/`);
+      ws.onopen = () => {
+        console.log('Connected');
+        this.sub();
+      };
+      ws.onmessage = (e) => {
+        console.debug('Receive');
+        lastJson = e.data;
+        try {
+          setData(JSON.parse(lastJson));
+        } catch (err) {
+          console.error('Error parsing JSON:', err);
+        }
+      };
+      ws.onerror = function (err) {
+        console.error('Error:', err.message);
+        ws.close();
+      };
+      ws.onclose = (e) => {
+        console.log('Disconnected:', e.reason);
+        clearIntervals();
+        setTimeout(connect, reconnectDelay);
+      };
+    };
+
+    const subKey = getParam(subKeyParam);
+    const key = `${appKey}:${subKey}`;
+
+    this.pub = () => {
+      if (!ws?.readyState === WebSocket.OPEN) return;
+      const data = getData();
+      const currentJson = JSON.stringify(data);
+      if (currentJson === lastJson) return;
+      console.debug('Publish');
+      ws.send(JSON.stringify({ action: 'pub', key, data }));
+      lastJson = currentJson;
+    };
+    this.sub = () => {
+      if (!ws?.readyState === WebSocket.OPEN) return;
+      ws.send(JSON.stringify({ action: 'sub', key }));
+    };
+
+    if (subKey) {
+      connect();
+    }
+  }
+}
+
+export default PubSub;
---
+++ server.js
@@ -0,0 +1,83 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const http = require('http');
+const NodeCache = require('node-cache');
+const WebSocket = require('ws');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: [/\.kvn\.pt$/] }));
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const cache = new NodeCache({ stdTTL: 3 * 24 * 3600 });
+const keyClients = {};
+
+// Helpers
+const clientAdd = (key, client) => {
+  keyClients[key] = keyClients[key] || new Set();
+  keyClients[key].add(client);
+};
+const clientDelete = (client) => {
+  for (let key in keyClients) {
+    const found = keyClients[key].delete(client);
+    if (found && keyClients[key].size === 0) {
+      delete keyClients[key];
+    }
+  }
+};
+const setAndPublish = (key, data, self = null) => {
+  cache.set(key, data);
+  const dataJson = JSON.stringify(data);
+  (keyClients[key] || []).forEach((client) => {
+    if (client !== self && client.readyState === WebSocket.OPEN) {
+      client.send(dataJson);
+    }
+  });
+};
+
+// HTTP handlers
+app.post('/cache/:key', (req, res) => {
+  if (!req.body) return res.status(400).json();
+  setAndPublish(req.params.key, req.body);
+  res.json();
+});
+app.get('/cache/:key', (req, res) => {
+  const data = cache.get(req.params.key);
+  res.json(data || null);
+});
+
+// WebSocket handlers
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', (message) => {
+    let parsed;
+    try {
+      parsed = JSON.parse(message);
+    } catch {}
+    if (!parsed || !parsed.key) return;
+
+    switch (parsed.action) {
+      case 'pub':
+        if (!parsed.data) return;
+        setAndPublish(parsed.key, parsed.data, ws);
+        break;
+      case 'sub':
+        clientAdd(parsed.key, ws);
+        const data = cache.get(parsed.key);
+        if (data) {
+          ws.send(JSON.stringify(data));
+        }
+        break;
+    }
+  });
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clientDelete(ws);
+  });
+});
+
+// Start server
+server.listen(8075, () => {
+  console.log('Server is running');
+});
